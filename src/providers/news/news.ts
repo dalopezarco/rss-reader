0af5192526e84d144da24817e3958166639177a7
Daniel Lopez <daniell@devanddel.com>
2019-01-10 20:18:49 +0100

    Sort news by date

--- src/providers/news/news.ts
+++ src/providers/news/news.ts
@@ -11,6 +11,12 @@ export class NewsProvider {
 
   getNews() {
     const newsUrl = 'https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Fep00.epimg.net%2Frss%2Felpais%2Fportada.xml';
-    return this.http.get(newsUrl).pipe(map((res: articlesResponse) => res.items));
+    return this.http.get(newsUrl).pipe(map((res: articlesResponse) => res.items.sort(this.compareDates)));
+  }
+
+  compareDates(articleA: article, articleB: article) {
+    let dateA = new Date(articleA.pubDate).getTime();
+    let dateB = new Date(articleB.pubDate).getTime();
+    return dateB - dateA;
   }
 }
